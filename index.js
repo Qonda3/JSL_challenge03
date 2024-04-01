46535636d6c4383c5d0644a890d4aca788db74b5
Qonda Mampofu <138157862+Qonda3@users.noreply.github.com>
2024-04-01 18:57:55 +0200

    Uploading files

---
+++ index.js
@@ -0,0 +1,15 @@
+
+firstName = 'John';
+age = 35;
+hobby = 'Coding';
+//Qonda
+const logTwice = () => {
+  console(parameter)
+  console(parameter)
+}
+
+function hobby () {
+  logTwice(`Hello, ${name} (${age}). I love ${hobby}!`)
+}
+
+hobby()
