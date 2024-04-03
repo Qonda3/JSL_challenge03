82d30ecb677803016bfb94d2cb94c490f767256a
Qonda <138157862+Qonda3@users.noreply.github.com>
2024-04-03 09:42:56 +0200

    debugging

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 
-firstName = 'John';
-age = 35;
-hobby = 'Coding';
+const firstName = 'John';
+const age = 35;
+const hobby = 'Coding';
 //Qonda
-const logTwice = () => {
-  console(parameter)
-  console(parameter)
+const logTwice = (parameter) => {
+  console.log(parameter);
+  console.log(parameter);
+};
+
+function hobbyFunc () {
+  logTwice(`Hello, ${firstName} (${age}). I love ${hobby}!`);
 }
 
-function hobby () {
-  logTwice(`Hello, ${name} (${age}). I love ${hobby}!`)
-}
-
-hobby()
+hobbyFunc();
